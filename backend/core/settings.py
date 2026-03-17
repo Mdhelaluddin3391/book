7bf4249a3202151f607e3f4872df46c2f758beb0
Mdhelaluddin3391 <Mdhelaluddin3391@gmail.com>
2026-03-17 16:51:39 +0530

    updtes for some security

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -10,24 +10,24 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g-n$7p$6i!2b7i98cwah@14po_4kpugn9690vu(+*x(5_)&&=6'
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG') == 'True'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] # Production mein isko apni domain se replace karein
+FRONTEND_URL = os.getenv('FRONTEND_URL')
 CORS_ALLOW_ALL_ORIGINS = True
-FRONTEND_URL = 'http://127.0.0.1:5500/frontend' # Isko apne live server port se match kar lein
 
 # Application definition
 
@@ -120,28 +120,19 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STRIPE_WEBHOOK_SECRET = 'whsec_YahanAapkiStripeWebhookSecretAayegi'
-
-STRIPE_SECRET_KEY = 'sk_test_YourStripeSecretKeyHere'
+STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 
 # PayPal ke credentials (jab aap PayPal developer account banayenge tab milenge)
-PAYPAL_CLIENT_ID = 'YourPayPalClientIdHere'
-PAYPAL_SECRET = 'YourPayPalSecretHere'
+PAYPAL_CLIENT_ID = os.getenv('PAYPAL_CLIENT_ID')
+PAYPAL_SECRET = os.getenv('PAYPAL_SECRET')
 
 
 
-
-# Email Settings (Gmail ka example)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
-# Yahan apna real Gmail address dalein
-EMAIL_HOST_USER = 'muhammadhelal228@gmail.com' 
-
-# Yahan apne Gmail ka "App Password" dalein (Normal password nahi chalega)
-# App password generate karne ke liye: Google Account -> Security -> 2-Step Verification -> App Passwords
-EMAIL_HOST_PASSWORD = 'upmv xedm hiwq oxod' 
-
-DEFAULT_FROM_EMAIL = 'muhammadhelal228@gmail.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
