7e47afc5e3a3a5a8ce9b81732883527b3fe89a42
Mdhelaluddin3391 <Mdhelaluddin3391@gmail.com>
2026-03-18 01:13:10 +0530

    some updates

--- frontend/config.js
+++ frontend/config.js
@@ -3,7 +3,5 @@
 const isLocalhost = window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost';
 
 const CONFIG = {
-    BACKEND_URL: isLocalhost 
-        ? "http://127.0.0.1:8000/api" 
-        : "https://book-27zu.onrender.com/api" // <-- Yahan apne Render backend ka URL daalna
+    BACKEND_URL:"https://book-27zu.onrender.com/api"
 };
